691abc32a22426281be306b7172e8ebfce5d7d51
AndresGallop <andresgallod.i@gmail.com>
2020-10-17 21:15:27 -0400

    Dynamic Data done Yeahgit add .git add .git add .git add .!

--- js/main.js
+++ js/main.js
@@ -4,12 +4,30 @@
     fetch('./data/classData.json')
     .then(res => res.json())//This line becomes json into js
     .then(data => {
+
+
+        let classinfo = document.querySelector(".profPanelText").children;
+            times = document.querySelector(".list-inline").children;
+
+            classinfo[0].innerHTML =  data.coursename + " - " + data.coursecode;
+            classinfo[1].innerHTML =  "Professor" + " - " + data.profname;
+
+            times[0].innerHTML =  data.classtime[0];
+            times[1].innerHTML =  data.classtime[1];
+
+    
+
         
-        debugger;
+        //debugger;
         console.log(data);
 
         //handleData
         // here is where you would call the function that puts the pieces on the page
+    
+        
+
+            
+
 
 
     })
